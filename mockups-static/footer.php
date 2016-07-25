21367305ae2eb98b076e67118b6c433e510cab2f
blaque <mike.kilmer@gmail.com>
2016-07-24 21:47:04 -0700

    First mockup commit
    This adds the basic front page

---
+++ mockups-static/footer.php
@@ -0,0 +1,13 @@
+<footer class="container-fluid">
+    <div class="container">
+        <nav class="navbar navbar-light">
+            <h2 class="navbar-brand">Trending Topics:</h2>
+            <ul class="nav navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Hillary vs. Bernie</a>
+              </li>
+
+            </ul>
+        </nav>
+    </div>
+</footer>
---
+++ mockups-static/header.php
@@ -0,0 +1,24 @@
+<header class="container-fluid">
+    <nav class="navbar navbar-light">
+      <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+        &#9776;
+      </button>
+      <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+        <a class="navbar-brand" href="#"> </a>
+        <ul class="nav navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Hot Debates</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">All Debates</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign-Up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</header>
